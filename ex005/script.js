2af07badbc7a185146cbb57e832de9282a474ad2
xavesplayer <Jeffersonpsalvador@hotmail.com>
2026-04-14 14:15:55 -0300

    Add initial HTML, CSS, and JavaScript files for DOM study project
    - Created ex05.html with basic structure and content for DOM learning.
    - Added script.js to manipulate DOM elements and demonstrate JavaScript functionality.
    - Introduced style.css for basic styling of the HTML elements.
    - Included a new image (DOM.webp) for visual reference in the project.

---
+++ ex005/script.js
@@ -0,0 +1,12 @@
+/*var p1 = window.document.getElementsByTagName('p') [0]
+window.alert(p1.innerText)
+p1.style.color = 'red'
+var corpo = window.document.body
+corpo.style.background = 'Grey'
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'Estou aguardando'
+document.getElementById('msg').innerText = 'Olá novamente'
+*/
+var d = document.querySelector('div#msg')
+d.style.color = 'blue'
